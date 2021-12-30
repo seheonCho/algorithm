d125e7b667e1b757c30d0606eff3cd5ec9669d0f
seheonCho <whtpgjs94@gmail.com>
2021-12-30 21:47:48 +0900

    백준1978

---
+++ src/boj/Quiz1978.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Quiz1978 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] numArr = new int[input];
+        for (int i = 0; i < input; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for(int numStr : numArr) {
+            if(isPrime(numStr)) cnt++;
+        }
+        System.out.println(cnt);
+
+    }
+
+    public static boolean isPrime(int num) {
+
+        if (num == 1) return false;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+
+        }
+        return true;
+    }
+
+}
