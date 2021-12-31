e272423bc785884989b990dc930c953f959a4eb2
seheonCho <whtpgjs94@gmail.com>
2021-12-31 21:41:51 +0900

    프로그래머스 월간코드챌린지시즌2 음양 더하기

---
+++ src/prgrms/월간코드챌린지시즌2/음양더하기.java
@@ -0,0 +1,26 @@
+package prgrms.월간코드챌린지시즌2;
+
+public class 음양더하기 {
+
+    public int solution(int[] absolutes, boolean[] signs) {
+
+        int answer = 0;
+
+        for (int i=0; i<absolutes.length; i++) {
+
+            if (!signs[i]) {
+                absolutes[i] = absolutes[i] * -1;
+
+            }
+
+        }
+
+        for (int num : absolutes) {
+            answer += num;
+        }
+
+        return answer;
+    }
+
+
+}
