78b8b2bf23132d80e62c02013cbc3277af0ae08c
seheonCho <whtpgjs94@gmail.com>
2022-01-01 15:24:31 +0900

    프로그래머스 Summer/Winter Coding(~2018) 소수 만들기

---
+++ src/prgrms/summer_Winter_Coding/소수만들기.java
@@ -0,0 +1,43 @@
+package prgrms.summer_Winter_Coding;
+
+public class 소수만들기 {
+
+    public int solution(int[] nums) {
+
+        int answer = 0;
+
+        for (int i=0; i<nums.length; i++) {
+
+            for (int j=i+1; j<nums.length; j++) {
+
+                for (int k=j+1; k<nums.length; k++) {
+                    if (isNotSosu(sumInt(nums[i], nums[j], nums[k]))) {
+                        answer++;
+                    }
+
+                }
+
+            }
+
+        }
+        return answer;
+    }
+
+    boolean isNotSosu(int sumNumber) {
+        for (int i=1; i<sumNumber+1; i++) {
+            if (i != 1 && i != sumNumber && sumNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int sumInt(int ...numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+}
