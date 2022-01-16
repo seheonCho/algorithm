a5f1a6efd94f9fa30de4bd6b727978d590c284e2
seheonCho <whtpgjs94@gmail.com>
2022-01-16 23:36:41 +0900

    프로그래머스 K번째수

---
+++ src/prgrms/정렬/K번째수.java
@@ -0,0 +1,29 @@
+package prgrms.정렬;
+
+import java.util.Arrays;
+
+public class K번째수 {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+
+            int numI, numJ, numK;
+
+            numI = commands[i][0];
+            numJ = commands[i][1];
+            numK = commands[i][2];
+
+            int[] temp = Arrays.copyOfRange(array, numI - 1, numJ);
+
+            Arrays.sort(temp);
+
+            answer[i] = temp[numK - 1];
+
+        }
+
+        return answer;
+    }
+
+}
