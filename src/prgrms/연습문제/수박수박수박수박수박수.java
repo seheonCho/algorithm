6b824fa94c37be14d91b4e07620632c59dd784e7
seheonCho <whtpgjs94@gmail.com>
2022-01-18 22:37:58 +0900

    프로그래머스 수박수박수박수박수박수?

---
+++ src/prgrms/연습문제/수박수박수박수박수박수.java
@@ -0,0 +1,41 @@
+package prgrms.연습문제;
+
+public class 수박수박수박수박수박수 {
+
+    StringBuilder sb = new StringBuilder();
+    final static String su = "수";
+    final static String bak = "박";
+
+    public String solution(int n) {
+        String answer = "";
+
+        for (int i=1; i<=n; i++) {
+
+            if (isEven(i)) {
+                addEvenTxt();
+            } else {
+                addOddTxt();
+            }
+
+        }
+        answer = sb.toString();
+        return answer;
+    }
+
+    private boolean isEven(int num) {
+        if (num == 0 || num % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addOddTxt() {
+         sb.append(su);
+    }
+
+    private void addEvenTxt() {
+         sb.append(bak);
+    }
+
+
+}
