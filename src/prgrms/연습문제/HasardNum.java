562d65b0892e0a8477cb6e422cb8dd41f8f672d0
seheonCho <whtpgjs94@gmail.com>
2022-01-23 22:12:11 +0900

    프로그래머스 하샤드 수

---
+++ src/prgrms/연습문제/HasardNum.java
@@ -0,0 +1,26 @@
+package prgrms.연습문제;
+
+public class HasardNum {
+
+    public boolean solution(int x) {
+        boolean answer = false;
+
+        int sum = 0;
+
+        for (String strNum : String.valueOf(x).split("")) {
+            sum += Integer.parseInt(strNum);
+        }
+
+        if (isZeroRemainder(x, sum)) {
+            answer = true;
+        }
+        return answer;
+    }
+
+    private boolean isZeroRemainder(int x, int sum) {
+        if (x % sum == 0) {
+            return true;
+        }
+        return false;
+    }
+}
