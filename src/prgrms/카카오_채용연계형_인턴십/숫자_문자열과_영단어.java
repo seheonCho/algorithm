25bbb277788d1396dad45fd8a015253a4907ac12
seheonCho <whtpgjs94@gmail.com>
2022-01-26 22:09:21 +0900

    프로그래머스 2021 카카오 채용연계형 인턴십

---
+++ src/prgrms/카카오_채용연계형_인턴십/숫자_문자열과_영단어.java
@@ -0,0 +1,24 @@
+package prgrms.카카오_채용연계형_인턴십;
+
+public class 숫자_문자열과_영단어 {
+
+    public int solution(String s) {
+        int answer = 0;
+        System.out.println(s);
+
+        String[] mappingEng
+                = new String[]{"zero","one","two","three","four",
+                               "five","six","seven","eight","nine"};
+        String[] mappingNum
+                = new String[]{"0","1","2","3","4",
+                               "5","6","7","8","9"};
+
+        for (int i = 0; i < mappingEng.length; i++) {
+            s = s.replaceAll(mappingEng[i], mappingNum[i]);
+        }
+
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}
