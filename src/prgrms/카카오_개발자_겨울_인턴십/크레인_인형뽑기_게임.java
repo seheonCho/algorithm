aaf40550580cb10807ba49ce3415ce61939c89b1
seheonCho <whtpgjs94@gmail.com>
2022-01-31 23:12:28 +0900

    프로그래머스 2021 카카오 개발자 겨울 인턴십 크레인 인형뽑기 게임

--- src/prgrms/카카오_개발자_겨울_인턴십/크레인_인형뽑기_게임.java
+++ src/prgrms/카카오_개발자_겨울_인턴십/크레인_인형뽑기_게임.java
@@ -1,98 +1,69 @@
 package prgrms.카카오_개발자_겨울_인턴십;
 
+import java.util.Stack;
+
 public class 크레인_인형뽑기_게임 {
 
     static int SIDE_LENGTH;
-    static int[] basket;
+    static Stack basket;
     static int[][] NEW_BOARD;
     static int BINGO_CNT = 0;
 
     public int solution(int[][] board, int[] moves) {
         SIDE_LENGTH = board.length;
-        basket = new int[moves.length];
+        basket = new Stack<Integer>();
         NEW_BOARD = board;
 
         for (int i = 0; i < moves.length; i++) {
-            getFirstNum(moves[i]);
+            getFirstNum(moves[i] - 1);
         }
 
-        System.out.println("last bingoCnt => " + BINGO_CNT);
         return BINGO_CNT;
     }
 
-    private void getFirstNum(int num) {
+    private void getFirstNum(int pickIndex) {
 
-        Loop1 :
         for (int i = 0; i < SIDE_LENGTH; i++) {
-            Loop2 :
-            for (int j = 0; j < SIDE_LENGTH; j++) {
-
-                if (isSame(NEW_BOARD[j][i], num)) {
-                    stackNum(num);
-                    // 이거지워야 될듯
-                    NEW_BOARD[j][i] = 0;
-                    break;
-                }
-
-            }
-
-        }
-
-    }
-
-    private boolean isSame(int a, int b) {
-        if (a == b) {
-            System.out.println("isSame => 숫자" + a + " / 숫자" + b);
-            return true;
-        }
-        return false;
-    }
-
-    // bingo Chk 로 해야할듯..
-    private void bingoChk() {
-
-        for (int i = 0; i < basket.length - 1; i++) {
-            if (isSame(basket[i], basket[i+1])) {
-                bingo(i, i+1);
-            }
-        }
-    }
-
-    private void bingo(int a, int b) {
-        basket[a] = 0;
-        basket[b] = 0;
-        BINGO_CNT++;
-        alignBasket(a);
-    }
-
-    private void alignBasket(int num) {
-        for (int i = num; i < basket.length - 1; i++) {
-            basket[num] = basket[num+1];
-        }
-        if (basket[basket.length-1] == 0) {
-            System.out.println("어떡하지");
-        }
-    }
-
-    private void stackNum(int num) {
-
-        for (int i = basket.length -1; i > -1; i--) {
-            if (i == 0 || basket[i - 1] != 0) {
-                System.out.println(basket[i]);
-                basket[i] = num;
-                System.out.println("진짜 쌓는다 => " + num);
-                for (int number : basket) {
-                    System.out.print(number);
-                }
-                System.out.println();
-                for (int number : basket) {
-                    System.out.print(number);
-                }
-                System.out.println();
+            if (NEW_BOARD[i][pickIndex] != 0) {
+                stackNum(NEW_BOARD[i][pickIndex]);
+                NEW_BOARD[i][pickIndex] = 0;
                 break;
             }
         }
 
+    }
+
+    private void stackNum(int num) {
+
+        if (basket.empty()) {
+            basket.push(num);
+        } else {
+            bingoChk(num);
+        }
+
+    }
+
+    private void bingoChk(int num) {
+
+        if (basket.peek().equals(num)) {
+            basket.pop();
+            addBingoCnt();
+        } else {
+            basket.push(num);
+        }
+
+    }
+
+    private void printBasket() {
+        System.out.println("현재 바스켓 => ");
+        for (Object number : basket) {
+            System.out.print(number);
+        }
+        System.out.println();
+    }
+
+    private void addBingoCnt() {
+        BINGO_CNT += 2;
     }
 
 }
