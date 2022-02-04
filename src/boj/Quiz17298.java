827090dd63d4e9dcd23d62fd3876aecc9f81b2ae
seheonCho <whtpgjs94@naver.com>
2022-02-04 10:12:22 +0900

    백준 17298 골드4 오큰수 38% 시간초과

---
+++ src/boj/Quiz17298.java
@@ -0,0 +1,47 @@
+package boj;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Quiz17298 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int theNumbers = Integer.parseInt(br.readLine());
+        String[] numbersStr = new String[theNumbers];
+
+        int[] outputNumbers = new int[theNumbers];
+
+        numbersStr = br.readLine().split(" ");
+        br.close();
+        int[] numbersInt = Arrays.stream(numbersStr).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < theNumbers - 1; i++) {
+
+            for (int j = i + 1; j < theNumbers; j++) {
+
+                if (numbersInt[i] < numbersInt[j]) {
+                    outputNumbers[i] = numbersInt[j];
+                    break;
+                    //break 로 나가야 한다, 최초 발견한 큰 수 중 가장 좌측이라서.
+                }
+
+                if (j == theNumbers-1 && outputNumbers[i] == 0) {
+                    outputNumbers[i] = -1;
+                }
+
+            }
+
+        }
+
+        outputNumbers[theNumbers-1] = -1;
+        for (int num : outputNumbers) bw.append(num + " ");
+        bw.flush();
+        bw.close();
+
+
+    }
+
+}
