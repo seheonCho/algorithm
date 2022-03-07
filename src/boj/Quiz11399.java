81478c724d2920f3be083bb659af488cb877df37
seheonCho <whtpgjs94@gmail.com>
2022-03-08 08:59:36 +0900

    백준 11399 실버3 그리디(탐욕법) ATM

---
+++ src/boj/Quiz11399.java
@@ -0,0 +1,39 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Quiz11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] times = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        Arrays.sort(times);
+
+        int sumTime = 0;
+        for (int i = 0; i < N; i++) {
+
+            for (int j = 0; i >= j; j++) {
+                sumTime += times[j];
+            }
+
+        }
+
+        System.out.println(sumTime);
+
+    }
+
+}
