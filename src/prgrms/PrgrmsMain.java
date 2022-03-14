f2beae9d20a14e3752968c66f7d9700d2514e738
seheonCho <whtpgjs94@gmail.com>
2022-03-14 21:37:45 +0900

    프로그래머스 main메서드 정리

--- src/prgrms/PrgrmsMain.java
+++ src/prgrms/PrgrmsMain.java
@@ -1,12 +1,8 @@
 package prgrms;
 
-import prgrms.level2.정렬.H_Index;
-
 public class PrgrmsMain {
 
     public static void main(String[] args) {
         // Used to programmer coding test solve
-        H_Index theHIndex = new H_Index();
-        System.out.println(theHIndex.solution(new int[]{3,0,6,1,5}));
     }
 }
