8ef0f7df14dd2b5b7e1ed8d3634b571237d6924d
seheonCho <whtpgjs94@gmail.com>
2022-03-18 20:40:58 +0900

    fix: 백준 10870 브론즈2 수학 피보나치 수

--- src/boj/수학/피보나치_수_5.java
+++ src/boj/수학/피보나치_수_5.java
@@ -11,9 +11,9 @@ public class 피보나치_수_5 {
     }
 
     private static int fibonacci(int number) {
-        if (number == 0) return 0;
-        if (number == 1) return 1;
-
+        if (number < 2) {
+            return number;
+        }
         return fibonacci(number - 1) + fibonacci(number - 2);
     }
 }
