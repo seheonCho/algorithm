f3d3afb3aff71f9ef8d6f49eddf5042e9ef96665
seheonCho <whtpgjs94@gmail.com>
2022-07-02 00:46:49 +0900

    백준 2581 실버5 수학 소수

--- src/boj/수학/소수.java
+++ src/boj/수학/소수.java
@@ -9,7 +9,6 @@ public class 소수 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int startBoundary = Integer.parseInt(br.readLine());
         int endBoundary = Integer.parseInt(br.readLine());
 
@@ -17,30 +16,32 @@ public class 소수 {
         int sum = 0;
 
         for (int i = startBoundary; i <= endBoundary; i++) {
+
             if (isPrime(i)) {
+
+                if (i == 1) continue;
+
                 primeNums.add(i);
                 sum += i;
             }
+
         }
 
         if (primeNums.size() == 0) {
-            bw.append("-1");
+            System.out.println("-1");
         } else {
             primeNums.sort((a, b) -> a - b);
+            System.out.println(sum);
             System.out.println(primeNums.get(0));
-            bw.append(sum + "\n");
         }
-        bw.flush();
-        bw.close();
         br.close();
-
 
     }
 
     private static boolean isPrime(int num) {
-        int sqrtNum = (int)Math.sqrt(num);
+        int sqrtNum = (int) Math.sqrt(num);
         for (int i = 2; i <= sqrtNum; i++) {
-            if (num % i == 0) {
+            if (num == 1 || num % i == 0) {
                 return false;
             }
         }
