0fa0558f0e1292b61b33300746c8aa74901b21da
seheonCho <whtpgjs94@gmail.com>
2022-11-24 16:43:50 +0900

    프로그래머스 레벨1 Summer/Winter Coding(~2018) 예산

---
+++ src/prgrms/level1/summer_Winter_Coding/예산.java
@@ -0,0 +1,25 @@
+package prgrms.level1.summer_Winter_Coding;
+
+import java.util.Arrays;
+
+public class 예산 {
+
+    public int solution(int[] d, int budget) {
+        int count = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget >= 0) {
+                count++;
+                continue;
+            }
+            break;
+        }
+
+        return count;
+    }
+
+}
